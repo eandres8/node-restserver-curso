043f1b479bc2363167aeb4ed5d1e3d2a137fbe2c
Andres Vega <elb.andres8@gmail.com>
2018-09-02 15:23:14 -0500

    variable de entorno MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -13,11 +13,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ============================================
 let urlDB;
 
-// if (process.env.NODE_ENV === 'dev') {
-//     urlDB = 'mongodb://localhost:27017/cafe';
-// } else {
-//     urlDB = 'mongodb://<cafeUser>:<a123456>@ds141952.mlab.com:41952/cafe';
-// }
-urlDB = 'mongodb://cafeUser:a123456@ds141952.mlab.com:41952/cafe';
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = process.env.MONGO_URI;
+}
 
 process.env.URL_DB = urlDB;
